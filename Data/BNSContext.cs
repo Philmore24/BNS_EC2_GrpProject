9d8dd562ccaf5917d5bd71cb9a6d6521e6a75e8f
Philmore Foster <fosterphilmore@outlook.com>
2022-11-08 17:51:02 -0500

    Add project files.

---
+++ Data/BNSContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BNS.Models;
+
+namespace BNS.Data
+{
+    public class BNSContext : DbContext
+    {
+        public BNSContext (DbContextOptions<BNSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BNS.Models.Accounts> Accounts { get; set; }
+    }
+}
